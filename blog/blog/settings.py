c2f92fe99aa11d7b79f734962c3e961162378d55
AkshayBenny <akshaybennygh@gmail.com>
2021-08-31 09:10:47 +0530

    error fixed

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5(m08)b$@4mf=r-q+*iz96b+3+vbc3ulu6ri+$c_p=p&ep-as-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
