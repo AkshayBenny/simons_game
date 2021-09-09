79b2f57081ef838fb5dabe00e7ad29be484fe7e6
AkshayBenny <akshaybennygh@gmail.com>
2021-09-09 19:29:57 +0530

    test js added

---
+++ script2.js
@@ -0,0 +1,8 @@
+function btnColorGenerator() {
+ 
+    const btnColors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red', 'pink', 'brown']
+    randNum = Math.floor(Math.random()*9)
+    return console.log(btnColors[randNum])
+    
+  }
+  
