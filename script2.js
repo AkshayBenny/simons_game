b0365670c24d4c3dcc7b214167cd3fdd17fa2978
AkshayBenny <akshaybennygh@gmail.com>
2021-09-09 20:51:14 +0530

    js update

--- script2.js
+++ script2.js
@@ -1,3 +1,5 @@
+var levelCounter = 0
+
 function btnColorGenerator() {
     var randColor
     const btnColors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red', 'pink', 'brown']
@@ -6,9 +8,26 @@ function btnColorGenerator() {
   }
   
 var returnColor = btnColorGenerator()
-  const btnArray = document.querySelectorAll('button')
-  for (var i=0; i<btnArray.length; i++){
+const btnArray = document.querySelectorAll('button')
+for (var i=0; i<btnArray.length; i++){
     if (btnArray[i].value == returnColor){
-      btnArray[i].classList.add("pressed")
+        btnArray[i].classList.add("pressed")
     }
+}//Highlights a random button
+
+var shownBtnArray = []
+shownBtnArray.push(returnColor)
+
+function pressedBtn() {
+	for (var j=0; j<btnArray.length; j++){
+  	  document.querySelectorAll("button")[j].addEventListener("click", function () {
+      	return this.value
+        })
   }
+}
+var pressedBtnArray = []
+pressedBtnArray.push(pressedBtn())
+console.log(pressedBtnArray)
+
+
+
