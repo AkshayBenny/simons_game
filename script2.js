ae2a6c42bb305f62bfc621635afd5a81325d5fda
AkshayBenny <akshaybennygh@gmail.com>
2021-09-09 20:14:54 +0530

    rand color generator function added

--- script2.js
+++ script2.js
@@ -1,8 +1,14 @@
 function btnColorGenerator() {
- 
+    var randColor
     const btnColors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red', 'pink', 'brown']
-    randNum = Math.floor(Math.random()*9)
-    return console.log(btnColors[randNum])
-    
+    var randNum = Math.floor(Math.random()*9)
+    return randColor = btnColors[randNum] 
   }
   
+var returnColor = btnColorGenerator()
+  const btnArray = document.querySelectorAll('button')
+  for (var i=0; i<btnArray.length; i++){
+    if (btnArray[i].value == returnColor){
+      btnArray[i].classList.add("pressed")
+    }
+  }
